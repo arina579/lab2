600dcbf9d13186a95d9c155b5321b9994aff50d8
arina579 <hopeislife@gmail.com>
2026-05-13 22:13:46 +0300

    Translate comments to English

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-// Программа для демонстрации персонализированного "Hello World"
+//Program to demonstrate personalized "Hello World" 
 // Исправлено: удалено 'using namespace std' для большей ясности кода
 int main()
 {    std::string name;  // Variable to store user's name
@@ -10,3 +10,4 @@ int main()
     // Display personalized greeting
     std::cout << "Hello world from " << name << std::endl;
         return 0;  // Program executed successfully}
+
